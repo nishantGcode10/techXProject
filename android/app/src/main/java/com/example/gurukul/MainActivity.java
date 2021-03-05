ccb14f678551a3296f252aeadd09d35dc08def32
Nishant Goel <goyalnishant72@gmail.com>
2021-03-06 00:24:35 +0530

    init commit

---
+++ android/app/src/main/java/com/example/gurukul/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.gurukul;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.light,
+        /* light theme settings */
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        /* dark theme settings */
+      ),
+      themeMode: ThemeMode.dark,
+    ));
+  }
+}
